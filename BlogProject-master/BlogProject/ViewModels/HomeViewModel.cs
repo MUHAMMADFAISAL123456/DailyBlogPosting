cc7ef1d8ef1a750f1ed0279aeda4fd1b60cb3b0d
muhammadfaisal123 <faisal6465@outlook.com>
2022-04-16 18:10:59 +0400

    updated

---
+++ BlogProject-master/BlogProject/ViewModels/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BlogProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogProject.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Post> PostsList { get; set; }
+
+        public int TotalPosts { get; set; }
+
+        public int PageNo { get; set; }
+        public int Posts { get; set; }
+
+        public bool DisplayNext { get; set; }
+        public bool DisplayPrevious { get; set; }
+    }
+}
